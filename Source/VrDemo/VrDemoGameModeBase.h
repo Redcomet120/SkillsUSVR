7993b0d359b5d9744fe448993d6b2fcfb3c41f30
Matthew Blancher <redcomet120@gmail.com>
2019-06-08 13:33:30 -0700

    initial commit

---
+++ Source/VrDemo/VrDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VrDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRDEMO_API AVrDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
